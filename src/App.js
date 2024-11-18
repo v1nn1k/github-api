90f3b51c2834ba2583fc63658eab0a8390184c68
an4ernik <an4ernik2@gmail.com>
2024-11-18 23:59:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  const getRepoByUsername =  () => {
+    fetch(`https://api.github.com/users/${inputValue}/repos`, {
+      method: "GET",
+    headers: {
+      Authorization: `forsto`,
+      "Content-Type": "application/json",
+    },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setRepos(json);
+      });}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello brother
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="input">
+        <input type="text" value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Put username here"/>
+        <button onClick={getRepoByUsername}>Show repos</button>
+      </div>
+      <div>
+    {repos.map((repo) => (
+      <div key={repo.id}>
+        <h3>{repo.name}</h3>
+        <p>{repo.description}</p>
+      </div>
+    ))}
+  </div>
+</div>
   );
 }
 
