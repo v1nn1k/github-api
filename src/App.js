ca18c41f6e8e22c509c32b846ea63ea032f1fc80
an4ernik <an4ernik2@gmail.com>
2024-11-21 22:08:15 +0200

    made changes according to  comments

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
 
   const getRepoByUsername =  () => {
     fetch(`https://api.github.com/users/${inputValue}/repos`, {
-      method: "GET",
     headers: {
       Authorization: `forsto`,
       "Content-Type": "application/json",
@@ -17,21 +16,21 @@ function App() {
     })
       .then((response) => response.json())
       .then((json) => {
-        console.log(json);
         setRepos(json);
       });}
   
   return (
     <div>
-      <div className="input">
+      <h1>GitHub Repository Explorer</h1>
+      <div>
         <input type="text" value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           placeholder="Put username here"/>
         <button onClick={getRepoByUsername}>Show repos</button>
       </div>
-      <div>
+      <div >
     {repos.map((repo) => (
-      <div key={repo.id}>
+      <div className='repos' key={repo.id}>
         <h3>{repo.name}</h3>
         <p>{repo.description}</p>
       </div>
